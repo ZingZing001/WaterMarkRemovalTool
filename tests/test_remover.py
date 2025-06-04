6b630ac079ea455f98d3616dc47f50dc53fc08fd
ZingZing_ <rzha972@aucklanduni.ac.nz>
2025-06-04 17:19:52 +1200

    Fix import order in test_remover.py to ensure sys.path is set correctly

--- tests/test_remover.py
+++ tests/test_remover.py
@@ -1,6 +1,8 @@
-import numpy as np
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
+import numpy as np
 import removerPdf
 import removerWord
 
